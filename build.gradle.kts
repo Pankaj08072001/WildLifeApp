c6813a2e00c954bae93163a08b805dd820464fc7
Pankaj08072001 <pk9278424@gmail.com>
2026-04-02 12:25:15 +0530

    Set MainActivity as the launcher activity and remove launcher icon resources

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.2") // ✅ FIXED
+        classpath("com.google.gms:google-services:4.4.2")
     }
 }
